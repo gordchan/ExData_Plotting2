dc6f9cc73ea88c54fa1aee9e60cbb5bb71f6444f
gordchan <gordon.chan@mensa.org.hk>
2015-08-07 02:01:55 +0000

    Completed

---
+++ plot2.R
@@ -0,0 +1,24 @@
+## Reading in datasets
+
+library(dplyr)
+
+NEI <- readRDS("summarySCC_PM25.rds")
+# SCC <- readRDS("Source_Classification_Code.rds")
+
+NEI <- tbl_df(NEI)
+# SCC <- tbl_df(SCC)
+
+## Subset of total PM2.5 emission per year for Baltimore City, Maryland
+
+ctPM2.5 <- NEI %>% filter(fips=="24510") %>% group_by(year) %>% summarise(sum(Emissions))
+
+names(ctPM2.5) <- c("year", "total")
+
+## Plot xy graph
+
+png(filename = "plot2.png")
+
+  plot(total~year, data=ctPM2.5, type = "l", xlab = "Year", ylab = "Total Emissions (Tons)")
+    title(main="Total PM2.5 Emissions per Year\n(Baltimore City, Maryland)")
+
+dev.off()
