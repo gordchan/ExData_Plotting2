942fefef8e15529d2dd35f831ba04d439b95c9ba
gordchan <gordon.chan@mensa.org.hk>
2015-08-07 01:45:07 +0000

    Completed

---
+++ plot1.R
@@ -0,0 +1,22 @@
+## Reading in datasets
+
+library(dplyr)
+
+NEI <- readRDS("summarySCC_PM25.rds")
+# SCC <- readRDS("Source_Classification_Code.rds")
+
+NEI <- tbl_df(NEI)
+# SCC <- tbl_df(SCC)
+
+## Summary table of total PM2.5 emission per year
+
+tPM2.5 <- NEI %>% group_by(year) %>% summarise(sum(Emissions))
+
+names(tPM2.5) <- c("year", "total")
+
+png(filename = "plot1.png")
+
+  plot(total~year, data=tPM2.5, type = "l", xlab = "Year", ylab = "Total Emissions (Tons)")
+    title(main="Total PM2.5 Emissions per Year")
+    
+dev.off()
