32457a64b5bb66ae81e86543a9b0e43e6003ef00
gordchan <gordon.chan@mensa.org.hk>
2015-08-07 04:48:15 +0000

    Changed to % change graph

--- plot6.R
+++ plot6.R
@@ -15,7 +15,17 @@ sVehicle <- SCC %>% mutate(Mobile.Vehicle = (grepl("(^Mobile.+Vehicles)", EI.Sec
 ## Subset Coal related source
 cmPM2.5 <- NEI %>% filter(SCC %in% sVehicle$SCC & (fips == "24510"|fips == "06037")) %>% group_by(fips, year) %>% summarise(sum(Emissions))
 
-names(cmPM2.5) <- c("county", "year", "total")
+## Convert to percentage change
+
+c1mPM2.5 <- cmPM2.5 %>% filter(fips == "06037")
+  pc1 <- c1mPM2.5$`sum(Emissions)`/c1mPM2.5$`sum(Emissions)`[1]
+
+c2mPM2.5 <- cmPM2.5 %>% filter(fips == "24510")
+  pc2 <- c2mPM2.5$`sum(Emissions)`/c2mPM2.5$`sum(Emissions)`[1]
+
+cmPM2.5 <- cbind(cmPM2.5, c((pc1*100)-100, (pc2*100)-100))
+
+  names(cmPM2.5) <- c("county", "year", "total", "percentage")
 
 ## Plot xy graph by type
 
@@ -23,10 +33,11 @@ library(ggplot2)
 
 png(filename = "plot6.png")
 
-ggplot(cmPM2.5, aes(x = year, y = total, color = county)) +
+ggplot(cmPM2.5, aes(x = year, y = percentage, color = county)) +
   geom_line(size=2) +
-  labs(x = "Year", y = "Total Emissions (Tons)") +
-  ggtitle("Total PM2.5 Emissions per Year from Motor Vehicle Sources") +
-  scale_colour_discrete(labels=c("Los Angeles County", "Baltimore City"))
+  labs(x = "Year", y = "% change in Total Emissions (Tons)") +
+  ggtitle("Changes in PM2.5 Emissions per Year\nfrom Motor Vehicle Sources") +
+  scale_colour_discrete(labels=c("Los Angeles County", "Baltimore City")) +
+  geom_hline(yintercept= 0, linetype = 2)
 
 dev.off()
